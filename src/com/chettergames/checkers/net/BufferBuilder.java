ca59b9e86f28ae7838be5e55c3eb8b862ae68942
Mike Chargo <mike.chargo@sbcglobal.net>
2015-01-06 14:37:24 -0600

    implmented pushInt, pullInt, pushFlag, pullFlag

--- src/com/chettergames/checkers/net/BufferBuilder.java
+++ src/com/chettergames/checkers/net/BufferBuilder.java
@@ -16,22 +16,49 @@ public class BufferBuilder
 	
 	public void pushInt(int i)
 	{
-		
+		byte b1 = (byte)(i>>24);
+		byte b2 = (byte)(i>>16);
+		byte b3 = (byte)(i>>8);
+		byte b4 = (byte)(i);
+		buffer[index]= b1;
+		buffer[index+1]= b2;
+		buffer[index+2]= b3;
+		buffer[index+3]= b4;
+		index+=4;
 	}
 	
-	public byte pullInt()
+	public int pullInt()
 	{
-		return 0;
+		int b1 = buffer[index];
+		int b2 = buffer[index+1];
+		int b3 = buffer[index+2];
+		int b4 = buffer[index+3];
+		
+		if(b1<0) b1+=256;
+		if(b2<0) b2+=256;
+		if(b3<0) b3+=256;
+		if(b4<0) b4+=256;
+		
+		int i1 = (byte)(b1<<24);
+		int i2 = (byte)(b2<<16);
+		int i3 = (byte)(b3<<8);
+		int i4 = (byte)(b4);
+		index+=4;
+		
+		return i1+i2+i3+i4;
 	}
 	
 	public void pushFlag(byte flag)
 	{
-		
+		buffer[index]=flag;
+		index++;
 	}
 	
 	public byte pullFlag()
 	{
-		return 0;
+		byte flag=buffer[index];
+		index++;
+		return flag;
 	}
 	
 	public void pushByteArr(byte[] arr)
