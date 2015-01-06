e6c77ef602e5c13a86775207b22737d9809a14dd
John Detter <john@detter.com>
2015-01-06 14:46:47 -0600

    Added BufferBuilder code

--- src/com/chettergames/checkers/net/BufferBuilder.java
+++ src/com/chettergames/checkers/net/BufferBuilder.java
@@ -36,32 +36,46 @@ public class BufferBuilder
 	
 	public void pushByteArr(byte[] arr)
 	{
-		
+		pushInt(arr.length);
+		for(int x = 0;x < arr.length;x++)
+			buffer[index + x] = arr[x];
+		index += arr.length;
 	}
 	
 	public byte[] pullByteArr()
 	{
-		return null;
+		byte[] arr = new byte[pullInt()];
+		
+		for(int x = 0;x < arr.length;x++)
+			arr[x] = buffer[index + x];
+		index += arr.length;
+		
+		return arr;
 	}
 	
 	public void pushString(String string)
 	{
-		
+		pushByteArr(string.getBytes());
 	}
 	
 	public String pullString()
 	{
-		return "";
+		return new String(pullByteArr());
 	}
 	
 	public void pushStringArr(String arr[])
 	{
-		
+		pushInt(arr.length);
+		for(String s : arr) pushString(s);
 	}
 	
 	public String[] pullStringArr()
 	{
-		return null;
+		String strings[] = new String[pullInt()];
+		for(int x = 0;x < strings.length;x++)
+			strings[x] = pullString();
+		
+		return strings;
 	}
 	
 	private byte[] buffer;
